f694fbc987d640d9c9facae95b41ba0672a48a90
alexquantum007@gmail.com <alexquantum007@gmail.com@3246f138-69dd-4bd9-9acb-39f7fea0ab32>
2011-10-03 01:21:23 +0000

    Ajout du code pour l'ArrayStack.

--- src/util/collection/arrayStack/ImplStack.java
+++ src/util/collection/arrayStack/ImplStack.java
@@ -1,113 +1,192 @@
-/**
- * 
- */
 package util.collection.arrayStack;
 
 import util.collection.Stack;
 
 /**
- * @author Christian
- *
+ * Implémentation de la classe Stack par un tableau.
+ * 
+ * Il s'agit d'une pile d'objets de type « dernier entré, premier sorti » (LIFO). 
+ * Elle n'accepte pas d'éléments marqués « null ».
+ * 
+ * @author Christian Lesage
+ * @author Alexandre Tremblay
+ * 
  */
 public class ImplStack extends Stack
 {
-
+	// Taille par défaut du tableau et valeur utilisé lors d'un agrandissement.
+	private final static int TABLE_SIZE = 5;
+	
+	// Taille de la pile.
+	private int size;
+	
+	// Tableau d'objet
+	private Object[] table;
+	
 	/**
 	 * Construit une pile vide.
 	 */
 	public ImplStack()
 	{
-		// TODO Auto-generated constructor stub
+		this.clear();
 	}
 
-    /* (non-Javadoc)
+	/* (non-Javadoc)
 	 * @see util.collection.Stack#clear()
 	 */
 	@Override
 	public void clear()
 	{
-		// TODO Auto-generated method stub
-
+		this.table = new Object[ImplStack.TABLE_SIZE];
+		this.size = 0;
 	}
 
-    /* (non-Javadoc)
+	/* (non-Javadoc)
 	 * @see util.collection.Stack#get(int)
 	 */
 	@Override
 	public Object get(int position)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		Object o = null;
+		
+		if (position >= 0 && position <= this.size)
+		{
+			o = this.table[position];
+		}
+		
+		return o;
 	}
 
-    /* (non-Javadoc)
+	/* (non-Javadoc)
 	 * @see util.collection.Stack#isEmpty()
 	 */
 	@Override
 	public boolean isEmpty()
 	{
-		// TODO Auto-generated method stub
-		return false;
+		return (this.size == 0);
 	}
 
-    /* (non-Javadoc)
+	/*
+	 * Agrandi le tableau de la pile de « TABLE_SIZE » unités.
+	 */
+	private void resize() {
+		Object[] newTable = new Object[this.table.length + ImplStack.TABLE_SIZE];
+		
+		for (int i = 0; i < size; i++)
+		{
+			newTable[i] = table[i];
+		}
+		
+		this.table = newTable;
+	}
+	
+	/*
+	 * Retourne true si le tableau de la pile est plein, sinon false.
+	 */
+	private boolean isFull() {
+		return (this.size >= this.table.length);
+	}
+	
+	/* (non-Javadoc)
 	 * @see util.collection.Stack#peek()
 	 */
 	@Override
 	public Object peek()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return this.get(this.size - 1);
 	}
 
-    /* (non-Javadoc)
+	/* (non-Javadoc)
 	 * @see util.collection.Stack#pop()
 	 */
 	@Override
 	public Object pop()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		Object o = null;
+		
+		if (!this.isEmpty())
+		{
+			o = this.peek();
+			this.size--;
+		}
+		
+		return o;
 	}
 
-    /* (non-Javadoc)
+	/* (non-Javadoc)
 	 * @see util.collection.Stack#push(java.lang.Object)
 	 */
 	@Override
 	public Object push(Object obj)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		if (obj != null)
+		{
+			//Si le tableau est plein, on redimensionne
+			if (isFull())
+			{
+				resize();
+			}
+			
+			//Ajoute l'élément au tableau
+			this.table[size] = obj;
+			this.size++;
+		}
+			
+		return obj;
 	}
 
-    /* (non-Javadoc)
+	/* (non-Javadoc)
 	 * @see util.collection.Stack#search(java.lang.Object)
 	 */
 	@Override
 	public int search(Object obj)
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		int pos = Stack.NULL_POSITION;
+        
+        if (obj != null) 
+        {
+                // Vérifie chacun des éléments de la pile.
+                for (int i = 0; i < this.size; i++)
+                {
+                        // Si l'objet recherché est égal à celui courant en i
+                        if (obj.equals(this.get(i)))
+                        {
+                                pos = i;
+                                break;
+                        }
+                }
+        }
+        
+        return pos;
 	}
 
-    /* (non-Javadoc)
+	/* (non-Javadoc)
 	 * @see util.collection.Stack#size()
 	 */
 	@Override
 	public int size()
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		return this.size;
 	}
 
-    /* (non-Javadoc)
+	/* (non-Javadoc)
 	 * @see util.collection.Stack#toString()
 	 */
 	@Override
 	public String toString()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		String s = new String();
+
+        for (int i = 1; i <= this.size; i++)
+        {
+                s += table[i].toString();
+                if (i <= size - 1)
+                {
+                        s += ", ";
+                }
+        }
+
+        return "[" + s + "]";
 	}
 
 }
